e6feac95d1bf832dcf55ab9a45cf088368ef78d3
Ajith-Kumar0408 <ajithak0093@gmail.com>
2023-02-15 23:38:55 +0530

    new tool

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Offline, Online } from "react-detect-offline";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import "./App.css";
 
-export default App;
+import AppPage from "./component/app-page";
+import ErrorPage from "./component/error-page";
+import GeneralPage from "./component/gen-page";
+
+const Screens = () => (
+  <Router>
+    <Switch>
+      <Route path="/app">
+        <>
+          <Offline>
+            <ErrorPage msg="Please Check your Internet Connection" />
+          </Offline>
+          <Online>
+            <AppPage />
+          </Online>
+        </>
+      </Route>
+      <Route component={GeneralPage} />
+    </Switch>
+  </Router>
+);
+
+export default Screens;
